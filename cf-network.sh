7c17d3f46e0a376e611ef60f48bd0d5838677df9
root <root@utu1604template.eng.vmware.com>
2017-06-29 02:27:44 +0000

    fix bugs

--- cf-network.sh
+++ cf-network.sh
@@ -11,13 +11,10 @@ cp bosh /usr/local/bin
 export BOSH_CA_CERT=/root/workspace/bosh-lite/ca/certs/ca.crt
 export BOSH_DEPLOYMENT=cf
 export BOSH_ENVIRONMENT=vbox
-bosh -e 192.168.50.6 --ca-cert <(bosh int ./creds.yml --path /director_ssl/ca) alias-env vbox
-wget https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent -O bosh-warden-boshlite-ubuntu-trusty-go_agent.tgz
-bosh -e vbox upload-stemcell  bosh-warden-boshlite-ubuntu-trusty-go_agent.tgz
+bosh -e 192.168.50.4  alias-env vbox
+bosh -e 192.168.50.4  login
+bosh -e vbox upload-stemcell  https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent
 bosh -e vbox update-cloud-config /root/workspace/cf-deployment/bosh-lite/cloud-config.yml -n
-
-wget -O cf-networking-release-1.0.0.tgz https://bosh.io/d/github.com/cloudfoundry-incubator/cf-networking-release
-bosh upload-release cf-networking-release-1.0.0.tgz
-
+bosh upload-release https://bosh.io/d/github.com/cloudfoundry-incubator/cf-networking-release
 bosh -e vbox -d cf deploy ~/workspace/cf-deployment/cf-deployment.yml --vars-store deployment-vars.yml -v system_domain=bosh-lite.com -o ~/workspace/cf-deployment/operations/experimental/use-cf-networking.yml -o ~/workspace/cf-deployment/operations/bosh-lite-no-pg.yml -n
 

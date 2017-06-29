2b40abb8ab07e66a23ea53466541876510ec42ca
root <root@utu1604template.eng.vmware.com>
2017-06-29 02:34:31 +0000

    fix bugs

--- cf-network.sh
+++ cf-network.sh
@@ -15,6 +15,6 @@ bosh -e 192.168.50.4  alias-env vbox
 bosh -e 192.168.50.4  login
 bosh -e vbox upload-stemcell  https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent
 bosh -e vbox update-cloud-config /root/workspace/cf-deployment/bosh-lite/cloud-config.yml -n
-bosh upload-release https://bosh.io/d/github.com/cloudfoundry-incubator/cf-networking-release
+bosh -e vbox upload-release https://bosh.io/d/github.com/cloudfoundry-incubator/cf-networking-release
 bosh -e vbox -d cf deploy ~/workspace/cf-deployment/cf-deployment.yml --vars-store deployment-vars.yml -v system_domain=bosh-lite.com -o ~/workspace/cf-deployment/operations/experimental/use-cf-networking.yml -o ~/workspace/cf-deployment/operations/bosh-lite-no-pg.yml -n
 

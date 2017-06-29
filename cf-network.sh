a21dc3daa4fb192b73419f0c4425038f19375b92
root <root@utu1604template.eng.vmware.com>
2017-06-29 02:34:31 +0000

    fix bugs

--- cf-network.sh
+++ cf-network.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/cloudfoundry/bosh-deployment ~/workspace/bosh-deployment
-git clone https://github.com/cloudfoundry/diego-release ~/workspace/diego-release
-git clone https://github.com/cloudfoundry/cf-deployment ~/workspace/cf-deployment
-git clone https://github.com/cloudfoundry-incubator/cf-networking-release ~/workspace/cf-networking-release
+[ ! -d "/root/workspace/bosh-deployment"  ] && git clone https://github.com/cloudfoundry/bosh-deployment ~/workspace/bosh-deployment
+[ ! -d "/root/workspace/diego-release"  ] && git clone https://github.com/cloudfoundry/diego-release ~/workspace/diego-release
+[ ! -d "/root/workspace/cf-deployment"  ] && git clone https://github.com/cloudfoundry/cf-deployment ~/workspace/cf-deployment
+[ ! -d "/root/workspace/cf-networking-release"  ] && git clone https://github.com/cloudfoundry-incubator/cf-networking-release ~/workspace/cf-networking-release
 wget https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-2.0.23-linux-amd64
 chmod +x bosh-cli-2.0.23-linux-amd64
 mv bosh-cli-2.0.23-linux-amd64 bosh
@@ -15,6 +15,6 @@ bosh -e 192.168.50.4  alias-env vbox
 bosh -e 192.168.50.4  login
 bosh -e vbox upload-stemcell  https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent
 bosh -e vbox update-cloud-config /root/workspace/cf-deployment/bosh-lite/cloud-config.yml -n
-bosh upload-release https://bosh.io/d/github.com/cloudfoundry-incubator/cf-networking-release
+bosh -e vbox upload-release https://bosh.io/d/github.com/cloudfoundry-incubator/cf-networking-release
 bosh -e vbox -d cf deploy ~/workspace/cf-deployment/cf-deployment.yml --vars-store deployment-vars.yml -v system_domain=bosh-lite.com -o ~/workspace/cf-deployment/operations/experimental/use-cf-networking.yml -o ~/workspace/cf-deployment/operations/bosh-lite-no-pg.yml -n
 
